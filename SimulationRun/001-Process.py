1f1edde8a1d463185fc2f0d0e61129eeba9999a5
Robert Reihs <robert.reihs@medunigraz.at>
2021-02-25 16:09:26 +0100

    Update errors

--- SimulationRun/001-Process.py
+++ SimulationRun/001-Process.py
@@ -143,9 +143,9 @@ def main():
     number_of_processes = 5
     task_list = Queue()
     processes = []
-    #datasetpath = "/opt/storage/testImageTiling/"
+    # datasetpath = "/opt/storage/testImageTiling/"
+    # outputpath = "/opt/storage/testImageTilingOutput_001/"
     datasetpath = "/home/reihsr/Documents/development/TestSlides/"
-    #outputpath = "/opt/storage/testImageTilingOutput_001/"
     outputpath = "/home/reihsr/Documents/development/TestSlides/testoutput/"
 
     for root, dirs, files in os.walk(datasetpath, topdown=False):
